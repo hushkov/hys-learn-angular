8adf562cc15a90dede218c2b7be25b51fb16868e
Alex Hashkov <alexander.gashkov@hys-enterprise.com>
2022-12-05 16:15:48 +0000

    product list page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
+import { nanoid } from 'nanoid';
 import { Component } from '@angular/core';
+
+interface Product {
+  id: string;
+  title: string;
+  price: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-hys';
+  productsPerPage: number = 8;
+  products: Product[] = this.getProducts(this.productsPerPage);
+
+  getProducts(productsPerPage: number): Product[] {
+    let products = [];
+    for (let i = 1; i <= productsPerPage; i += 1) {
+      products.push({
+        id: nanoid(),
+        title: `Product #${i}`,
+        price: Math.floor(Math.random() * 100),
+      });
+    }
+    return products;
+  }
 }
